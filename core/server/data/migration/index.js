0d98edcdd2834c59b9d17e870288c64ae73ee484
Hannah Wolfe <erisds@gmail.com>
2014-07-10 16:10:00 +0100

    Migration improvements
    refs #2600
    
    - fixed issue with defaults not being populated on upgrade
    - added logging to all actions in the migration process to help debugging
      in future

--- core/server/data/migration/index.js
+++ core/server/data/migration/index.js
@@ -21,11 +21,16 @@ var _               = require('lodash'),
     migrateUp,
     migrateUpFreshDb;
 
+function logInfo(message) {
+    errors.logInfo('Migrations', message);
+}
+
 function getDeleteCommands(oldTables, newTables) {
     var deleteTables = _.difference(oldTables, newTables);
     if (!_.isEmpty(deleteTables)) {
         return _.map(deleteTables, function (table) {
             return function () {
+                logInfo('Deleting table: ' + table);
                 return utils.deleteTable(table);
             };
         });
@@ -37,6 +42,7 @@ function getAddCommands(oldTables, newTables) {
     if (!_.isEmpty(addTables)) {
         return _.map(addTables, function (table) {
             return function () {
+                logInfo('Creating table: ' + table);
                 return utils.createTable(table);
             };
         });
@@ -46,9 +52,10 @@ function getAddCommands(oldTables, newTables) {
 function addColumnCommands(table, columns) {
     var columnKeys = _.keys(schema[table]),
         addColumns = _.difference(columnKeys, columns);
-    
+
     return _.map(addColumns, function (column) {
         return function () {
+            logInfo('Adding column: ' + table + '.' + column);
             utils.addColumn(table, column);
         };
     });
@@ -60,12 +67,14 @@ function modifyUniqueCommands(table, indexes) {
         if (schema[table][column].unique && schema[table][column].unique === true) {
             if (!_.contains(indexes, table + '_' + column + '_unique')) {
                 return function () {
+                    logInfo('Adding unique on: ' + table + '.' + column);
                     return utils.addUnique(table, column);
                 };
             }
         } else if (!schema[table][column].unique) {
             if (_.contains(indexes, table + '_' + column + '_unique')) {
                 return function () {
+                    logInfo('Dropping unique on: ' + table + '.' + column);
                     return utils.dropUnique(table, column);
                 };
             }
@@ -85,11 +94,13 @@ init = function () {
         var defaultVersion = versioning.getDefaultDatabaseVersion();
         if (databaseVersion === defaultVersion) {
             // 1. The database exists and is up-to-date
+            logInfo('Up to date at version ' + databaseVersion);
             return when.resolve();
         }
         if (databaseVersion < defaultVersion) {
             // 2. The database exists but is out of date
             // Migrate to latest version
+            logInfo('Database upgrade required from version ' + databaseVersion + ' to ' +  defaultVersion);
             return self.migrateUp().then(function () {
                 // Finally update the databases current version
                 return versioning.setDatabaseVersion();
@@ -107,6 +118,7 @@ init = function () {
         if (err.message || err === 'Settings table does not exist') {
             // 4. The database has not yet been created
             // Bring everything up from initial version.
+            logInfo('Database initialisation required for version ' + versioning.getDefaultDatabaseVersion());
             return self.migrateUpFreshDb();
         }
         // 3. The database exists but the currentVersion setting does not or cannot be understood
@@ -133,15 +145,20 @@ migrateUpFreshDb = function () {
     var tables = [];
     tables = _.map(schemaTables, function (table) {
         return function () {
+            logInfo('Creating table: ' + table);
             return utils.createTable(table);
         };
     });
-
+    logInfo('Creating tables...');
     return sequence(tables).then(function () {
         // Load the fixtures
-        return fixtures.populateFixtures().then(function () {
-            // Initialise the default settings
-            return models.Settings.populateDefaults();
+        logInfo('Populating fixtures');
+        return fixtures.populateFixtures();
+    }).then(function () {
+        // Initialise the default settings
+        logInfo('Populating default settings');
+        return models.Settings.populateDefaults().then(function () {
+            logInfo('Complete');
         });
     });
 };
@@ -169,7 +186,9 @@ function backupDatabase() {
         // Save the exported data to the file system for download
         var fileName = path.resolve(config().paths.contentPath + '/data/exported-' + (new Date().getTime()) + '.json');
 
-        return nodefn.call(fs.writeFile, fileName, JSON.stringify(exportedData));
+        return nodefn.call(fs.writeFile, fileName, JSON.stringify(exportedData)).then(function () {
+            logInfo('Database backup written to: ' + fileName);
+        });
     });
 }
 
@@ -183,6 +202,7 @@ migrateUp = function () {
         modifyUniCommands = [],
         commands = [];
 
+    logInfo('Creating database backup');
     return backupDatabase().then(function () {
         return utils.getTables().then(function (tables) {
             oldTables = tables;
@@ -232,11 +252,19 @@ migrateUp = function () {
         }
         // execute the commands in sequence
         if (!_.isEmpty(commands)) {
+            logInfo('Running migrations');
             return sequence(commands);
         }
         return;
     }).then(function () {
+        logInfo('Updating fixtures');
         return fixtures.updateFixtures();
+    }).then(function () {
+        // Initialise the default settings
+        logInfo('Populating default settings');
+        return models.Settings.populateDefaults().then(function () {
+            logInfo('Complete');
+        });
     });
 };
 
